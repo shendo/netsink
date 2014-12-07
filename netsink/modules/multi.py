24f9862c36feeaae7f323733a3081f23c3419ac8
shendo <steve.henderson@hendotech.com.au>
2014-12-07 11:23:50 +1100

    Fix tight cpu loop when handling small packets in dispatcher.

--- netsink/modules/multi.py
+++ netsink/modules/multi.py
@@ -1,6 +1,6 @@
 # Netsink - Network Sinkhole for Isolated Malware Analysis
 # Copyright (C) 2013-2014 Steve Henderson
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -16,6 +16,7 @@
 
 import inspect
 import logging
+import select
 import socket
 import time
 
@@ -28,12 +29,12 @@ log = logging.getLogger(__name__)
 class Dispatcher(StreamHandler):
     version = __version__
     handlers = []
-    
+
     def config(self, config):
         from netsink.modules import registry
-        self.min_inspect = 20 # minimum bytes needed to match with
-        self.read_timeout = 10 # in seconds
-        
+        self.min_inspect = 20  # minimum bytes needed to match with
+        self.read_timeout = 10  # in seconds
+
         # set the config statically once, so we can modify reference
         # on self.server for other handlers
         if not Dispatcher.handlers:
@@ -42,12 +43,13 @@ class Dispatcher(StreamHandler):
                     Dispatcher.handlers.append(registry[x.strip()])
                 except (LookupError, ImportError):
                     log.warn("Dispatcher unable to import handler module: %s", x)
-        # copy list for this instance 
+        # copy list for this instance
         self.handlers = list(Dispatcher.handlers)
-    
+
     def handle(self):
         data = ""
         num_timeouts = 0
+        last_size = 0
         while True:
             try:
                 # set a timeout in case client is waiting for server
@@ -57,15 +59,18 @@ class Dispatcher(StreamHandler):
                 # conn closed?
                 if not len(data):
                     break
-                if len(data) < self.min_inspect:
+                if last_size < len(data) < self.min_inspect:
+                    last_size = len(data)
+                    # wait for further activity
+                    select.select([self.connection], [], [], 0.1)
                     continue
                 log.debug("Peeking at data: %s", repr(data))
                 # switch back to blocking so handlers can use rfile/wfile
                 self.connection.settimeout(None)
                 num_timeouts = 0
-                # now find right handler to consume it                
+                # now find right handler to consume it
                 self.dispatch(data)
-                
+
             except socket.timeout:
                 # Unable to read min data size within timeout
                 # fallback to generic response/handler
@@ -79,7 +84,7 @@ class Dispatcher(StreamHandler):
                 self.connection.sendall("220 Hello from netsink?\r\n")
             time.sleep(0.1)
         log.debug("Dispatcher closing socket to client %s", self.client_address)
-            
+
     def dispatch(self, data):
         from netsink.modules import registry
         for x in self.handlers:
@@ -89,14 +94,14 @@ class Dispatcher(StreamHandler):
                     # instantiate and copy attributes
                     for name, cls in registry.items():
                         if x == cls:
-                            self.server.cfg = ModuleConfig("%s.conf" % name).cfg                    
+                            self.server.cfg = ModuleConfig("%s.conf" % name).cfg
                     handler = x(self.request, self.client_address, self.server)
                     # copy back as handler may have wrapped/modified
                     # the connection.. eg. upgrading to ssl/tls
                     self.connection = handler.connection
                     self.request = handler.connection
                     # keep reference for stateful use if needed later
-                    # in same conversation                    
+                    # in same conversation
                     self.handlers.insert(0, handler)
                 else:
                     # existing instance, just copy over latest attributes
@@ -110,9 +115,5 @@ class Dispatcher(StreamHandler):
                 return
         # default for no match.. just try to consume?
         self.connection.recv(2048)
-        log.warning("Could not find handler to match traffic, consuming. %s", 
+        log.info("Could not find handler to match traffic, consuming. %s",
                     repr(data))
-            
-                
-        
-    
---
+++ tests/test_multi.py
@@ -0,0 +1,82 @@
+# Netsink - Network Sinkhole for Isolated Malware Analysis
+# Copyright (C) 2013-2014 Steve Henderson
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import socket
+import SocketServer
+import thread
+import urllib2
+
+from netsink.config import ModuleConfig
+from netsink.modules import multi, http, sslwrap, registry
+
+# registry won't be setup unless package installed
+registry['http'] = http.HTTPHandler
+registry['ssl'] = sslwrap.SSLHandler
+
+def test_dispatched_http():
+    server = SocketServer.TCPServer(('', 0), multi.Dispatcher)
+    server.cfg = ModuleConfig('dispatcher.conf').cfg
+    thread.start_new_thread(server.serve_forever, ())
+    resp = urllib2.urlopen("http://127.0.0.1:{0}/anything/blah.html".format(
+                            server.socket.getsockname()[1])).read()
+    assert "Netsink" in resp
+
+def test_dispatched_https():
+    server = SocketServer.TCPServer(('', 0), multi.Dispatcher)
+    server.cfg = ModuleConfig('dispatcher.conf').cfg
+    thread.start_new_thread(server.serve_forever, ())
+    resp = urllib2.urlopen("https://127.0.0.1:{0}/anything/blah.html".format(
+                            server.socket.getsockname()[1])).read()
+    assert "Netsink" in resp
+
+def test_no_match():
+    server = SocketServer.TCPServer(('', 0), multi.Dispatcher)
+    server.cfg = ModuleConfig('dispatcher.conf').cfg
+    thread.start_new_thread(server.serve_forever, ())
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', server.socket.getsockname()[1]))
+    # server should just consume unknown client traffic
+    s.send('asdhakdfhkajfafdhjagsdfjjhsadfhjagsd1234234123412342134asddf' * 10)
+    s.close()
+
+def test_server_initiated():
+    server = SocketServer.TCPServer(('', 0), multi.Dispatcher)
+    server.cfg = ModuleConfig('dispatcher.conf').cfg
+    thread.start_new_thread(server.serve_forever, ())
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', server.socket.getsockname()[1]))
+    # client is expecting server to initiate comms
+    # check that server eventually gives up and sends
+    # something back in an attempt to elicit something from client
+    resp = s.recv(2048)
+    assert 'netsink' in resp
+    s.close()
+
+def test_small_data():
+    server = SocketServer.TCPServer(('', 0), multi.Dispatcher)
+    server.cfg = ModuleConfig('dispatcher.conf').cfg
+    thread.start_new_thread(server.serve_forever, ())
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('127.0.0.1', server.socket.getsockname()[1]))
+    # multiple sends but still less than needed for pattern matching
+    s.send('asdfk')
+    s.send('sasd')
+    s.send('00')
+    # check that server eventually gives up on the small data and sends
+    # something back in an attempt to elicit more comms
+    resp = s.recv(2048)
+    assert 'netsink' in resp
+    s.close()
